a81098f4df203a9dfcaf5e3a02874ce361c84279
jagadeshwaran <76894794+jagadeshwarandev@users.noreply.github.com>
2021-05-01 01:27:11 -0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,7 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCCDu9DwO_wSqdnkLxz9-3RLyAu6grBJhM",
-  authDomain: "travelgallery-d504b.firebaseapp.com",
-  projectId: "travelgallery-d504b",
-  storageBucket: "travelgallery-d504b.appspot.com",
-  messagingSenderId: "911497793140",
-  appId: "1:911497793140:web:24b2e3b3e8459781500b21"
+  //api key comes here
 };
 
 // Initialize Firebase
